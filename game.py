f73b9e3d6231c37dd70c3ee110999c044cb1e0e9
Irina <lykin4ik@gmail.com>
2022-12-13 22:50:16 +0200

    File to run

---
+++ game.py
@@ -0,0 +1,44 @@
+from game_exceptions import GameOver, EnemyDown, InvalidInput
+from models import Player, Enemy
+from settings import GameOptions
+
+
+def play():
+    while True:
+        username = input("Enter your name: ")
+        if len(username.strip()):
+            break
+        print("\nInvalid input \n" "You must enter your name")
+    while True:
+        try:
+            action = GameOptions.commands(input('Enter command: '))
+            if action == "start":
+                break
+        except InvalidInput:
+            print("\nInvalid input")
+    player = Player(username)
+    level = 1
+    enemy_obj = Enemy(level)
+    while True:
+        try:
+            print()
+            player.attack(enemy_obj)
+            print()
+            player.defence(enemy_obj)
+        except EnemyDown:
+            player.score += GameOptions.player_score_level_up
+            level += 1
+            enemy_obj = Enemy(level)
+            print(f"\nLevel {level}")
+
+
+if __name__ == '__main__':
+    try:
+        play()
+    except GameOver:
+        GameOver.save_game_result()
+        print("\nGame Over")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("\nGood bye!")
